c0feb7d31ccff602527a1f2774396e374d29468b
rabbi640 <mdmahfuz640@gmail.com>
2023-03-08 14:34:06 +0600

    2nd commit

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    expression = request.form['expression']
+    result = eval(expression)
+    return render_template('index.html', result=result)
